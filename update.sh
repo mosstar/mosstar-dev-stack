7355ada03efb828e64d484c82bef988aeaf0d41a
Mücahit Nezir <mucahit.nezir@mosstar.com.tr>
2024-07-03 10:38:36 +0300

    change update.sh file

--- update.sh
+++ update.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# find the directory of the script
+SCRIPT_DIR=$(dirname "$(realpath "$0")")
+cd "$SCRIPT_DIR" || exit  # change to the script directory
+
+# check .git directory exists
+if [ ! -d .git ]; then
+    echo "Git repository not found. Please clone the repository and try again."
+    exit 1
+fi
+
+# update the repository
 git checkout master
 git pull origin master
 
