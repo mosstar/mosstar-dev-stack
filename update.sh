766e51063f3c3563ee63b6e735e3a445452c76d8
oozcitak <oozcitak@gmail.com>
2026-02-27 12:47:59 +0300

    Fix update script

--- update.sh
+++ update.sh
@@ -7,12 +7,14 @@ usage() {
     echo "  --with-build    Restart docker compose with build"
     echo "  --recreate-env  Recreate .env file from .env.sample"
     echo "  --no-git-pull   Skip pulling the repository"
+    echo "  --tld <value>   Set the domain TLD (default: local), used with --recreate-env option"
     exit 0
 }
 
 # Parse command line options
 SKIP_BUILD=true
 SKIP_PULL=false
+LOCAL_TLD="local"
 RECREATE_ENV_FILE=false
 
 while [[ "$#" -gt 0 ]]; do
@@ -21,6 +23,7 @@ while [[ "$#" -gt 0 ]]; do
         --with-build) SKIP_BUILD=false; shift ;;
         --recreate-env) RECREATE_ENV_FILE=true; shift ;;
         --no-git-pull) SKIP_PULL=true; shift ;;
+        --tld) LOCAL_TLD="$2"; shift 2 ;;
         *) echo "Unknown option: $1"; usage ;;
     esac
 done
@@ -45,6 +48,10 @@ fi
 # recreate the .env file if requested
 if [ "$RECREATE_ENV_FILE" = true ]; then
     cp .env.sample .env
+    # Add TLD to environment file
+    echo "" >> .env
+    echo "# local domain" >> .env
+    echo "LOCAL_TLD=$LOCAL_TLD" >> .env
 fi
 
 # restart the docker containers with or without build
